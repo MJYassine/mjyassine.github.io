54396fa7e58fc6cdcb7a827d3428d2b96779e291
MJYassine <74205152+MJYassine@users.noreply.github.com>
2024-02-04 21:15:06 -0500

    final push

---
+++ projects/part3/Contact/script.js
@@ -0,0 +1,8 @@
+function menuFunction() {
+    var menu = document.getElementById("nav");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+}
